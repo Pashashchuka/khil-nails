a2d9bba4849656f1f98aa2db441deae34800229b
JDM <shchuka_pasha@mail.ru>
2023-03-04 01:09:37 +0300

    feat: implement portfolio section

--- khil-nails/src/components/PortfolioSection/PortfolioSection.tsx
+++ khil-nails/src/components/PortfolioSection/PortfolioSection.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import catIcon from '../../assets/icons/cat.svg'
+import zlataPhoto from '../../assets/images/photo-zlata.JPG'
+
 import styles from './PortfolioSection.module.scss'
 
 const PortfolioSection = () => {
@@ -8,6 +11,22 @@ const PortfolioSection = () => {
       <h1 className={styles.title}>
         Мои <span className={styles.titleWord}>работы</span>
       </h1>
+      <div className={styles.photoBlock}>
+        <div className={styles.photoLine}>
+          <img className={styles.widePhoto} src={zlataPhoto} alt="nails" />
+          <img className={styles.photo} src={zlataPhoto} alt="nails" />
+        </div>
+        <div className={styles.photoLine}>
+          <img className={styles.photo} src={zlataPhoto} alt="nails" />
+          <img className={styles.widePhoto} src={zlataPhoto} alt="nails" />
+        </div>
+        <div className={styles.photoLine}>
+          <img className={styles.widePhoto} src={zlataPhoto} alt="nails" />
+          <img className={styles.photo} src={zlataPhoto} alt="nails" />
+        </div>
+      </div>
+      <button className={styles.btn}>Больше работ</button>
+      <img className={styles.cat} src={catIcon} alt="cat" />
     </section>
   )
 }
