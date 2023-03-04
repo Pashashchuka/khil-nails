3ccc0dd5b9fca852f31294bc17bfe9b2fdb62258
JDM <shchuka_pasha@mail.ru>
2023-03-04 22:47:04 +0300

    feat: implement advantages section

--- khil-nails/src/components/AdvantagesSection/AdvantagesSection.tsx
+++ khil-nails/src/components/AdvantagesSection/AdvantagesSection.tsx
@@ -1,4 +1,8 @@
 import { FC } from 'react'
+import DoneAllIcon from '@mui/icons-material/DoneAll'
+import AccessTimeIcon from '@mui/icons-material/AccessTime'
+import SanitizerOutlinedIcon from '@mui/icons-material/SanitizerOutlined'
+import ThumbUpOffAltOutlinedIcon from '@mui/icons-material/ThumbUpOffAltOutlined'
 
 import catIcon from '../../assets/icons/cat.svg'
 
@@ -11,33 +15,30 @@ const AdvantagesSection: FC = () => {
         Почему выбирают <span className={styles.titleWord}>меня</span>
       </h1>
       <div className={styles.advantagesBlock}>
-        <div className={styles.lineBlock}>
-          <div className={styles.line}>
-            <div className={styles.card}>
-              <p className={styles.text}>
-                Выполняю работу быстро и качественно
-              </p>
-            </div>
-            <div className={styles.arrow}></div>
-            <div className={styles.card}>
-              <p className={styles.text}>
-                В работе использую только профессиональные инструменты
-              </p>
-            </div>
+        <div className={styles.line}>
+          <div className={styles.card}>
+            <p className={styles.text}>Выполняю работу быстро и качественно</p>
+            <AccessTimeIcon className={styles.icon} />
           </div>
-          <div className={styles.line}>
-            <div className={styles.card}>
-              <p className={styles.text}>Реализую любой дизайн на ваш вкус</p>
-            </div>
-            <div className={styles.arrow}></div>
-            <div className={styles.card}>
-              <p className={styles.text}>
-                Все инструменты проходят двухэтапную стерилизацию
-              </p>
-            </div>
+          <div className={styles.card}>
+            <p className={styles.text}>
+              В работе использую только профессиональные инструменты
+            </p>
+            <DoneAllIcon className={styles.icon} />
           </div>
         </div>
-        <div className={styles.asideArrow}></div>
+        <div className={styles.line}>
+          <div className={styles.card}>
+            <p className={styles.text}>Реализую любой дизайн на ваш вкус</p>
+            <ThumbUpOffAltOutlinedIcon className={styles.icon} />
+          </div>
+          <div className={styles.card}>
+            <p className={styles.text}>
+              Все инструменты проходят двухэтапную стерилизацию
+            </p>
+            <SanitizerOutlinedIcon className={styles.icon} />
+          </div>
+        </div>
       </div>
       <img className={styles.cat} src={catIcon} alt="cat" />
     </section>
