273122840ce71ad8e4499df05180b5bfaee94d62
JDM <shchuka_pasha@mail.ru>
2023-03-04 23:11:12 +0300

    feat: implement footer section

--- khil-nails/src/components/Footer/Footer.tsx
+++ khil-nails/src/components/Footer/Footer.tsx
@@ -1,7 +1,32 @@
 import { FC } from 'react'
+import TelegramIcon from '@mui/icons-material/Telegram'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined'
+
+import styles from './Footer.module.scss'
 
 const Footer: FC = () => {
-  return <div>Footer</div>
+  return (
+    <div className={styles.wrapper}>
+      <p className={styles.text}>Злата Хильмон</p>
+      <div className={styles.block}>
+        <LocationOnOutlinedIcon className={styles.icon} />
+        <p className={styles.text}>Минск, Беларусь</p>
+      </div>
+      <div className={styles.block}>
+        <InstagramIcon className={styles.icon} />
+        <a className={styles.link} href="advantages">
+          Instagram
+        </a>
+      </div>
+      <div className={styles.block}>
+        <TelegramIcon className={styles.icon} />
+        <a className={styles.link} href="price">
+          Telegram
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
