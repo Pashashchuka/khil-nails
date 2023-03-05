6f21095b9b9114025495045de83671bc85bd83e7
JDM <shchuka_pasha@mail.ru>
2023-03-05 16:51:08 +0300

    feat: add i18next library and locales folder

--- khil-nails/src/index.tsx
+++ khil-nails/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
+import '../src/i18next'
 import './scss/styles.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
